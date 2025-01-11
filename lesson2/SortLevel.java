03c8c970a860761d51081e444223e03775696ade
Alexey Bosalyga <alexey.bos@gmail.com>
2025-01-11 18:41:37 +0400

    Add files via upload

---
+++ lesson2/SortLevel.java
@@ -0,0 +1,18 @@
+package org.skillsmart.asd2.lesson2;
+
+import java.util.*;
+
+public class SortLevel {
+    public static void InsertionSortStep(int[] array, int step, int i)
+    {
+        for (int j = i + step; j < array.length; j += step) {
+            int tmpForExchange = array[j];
+            for (int k = j - step; k >= i && array[k] > tmpForExchange; k -= step) {
+                array[k + step] = array[k];
+                array[k] = tmpForExchange;
+            }
+        }
+    }
+}
+
+
